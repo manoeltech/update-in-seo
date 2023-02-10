840ddcbc0d8842f495a37f6cd355f63ae4b361aa
Manoel Pereira <manoel.programador21@gmail.com>
2023-02-10 11:48:56 -0300

    include site in config/sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://www.instagram.com/manoel.odesigner/"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
